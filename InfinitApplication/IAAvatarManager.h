e52253e2154eccb646d333c41b402c34130b1c87
Christopher Crone <christopher.crone@infinit.io>
2013-07-31 10:44:18 +0200

    Refactor of old Avatar Manager.

---
+++ InfinitApplication/IAAvatarManager.h
@@ -0,0 +1,13 @@
+//
+//  IAAvatarManager.h
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 7/31/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IAAvatarManager : NSObject
+
+@end
---
+++ InfinitApplication/IAAvatarManager.m
@@ -0,0 +1,13 @@
+//
+//  IAAvatarManager.m
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 7/31/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import "IAAvatarManager.h"
+
+@implementation IAAvatarManager
+
+@end
