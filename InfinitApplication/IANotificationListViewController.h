3376387d91908695785665b29a1cb3a1ea13a65a
Christopher Crone <christopher.crone@infinit.io>
2013-07-31 12:38:10 +0200

    Notification List initial commit.

---
+++ InfinitApplication/IANotificationListViewController.h
@@ -0,0 +1,13 @@
+//
+//  IANotificationListViewController.h
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 7/31/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IANotificationListViewController : NSViewController
+
+@end
---
+++ InfinitApplication/IANotificationListViewController.m
@@ -0,0 +1,27 @@
+//
+//  IANotificationListViewController.m
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 7/31/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import "IANotificationListViewController.h"
+
+@interface IANotificationListViewController ()
+
+@end
+
+@implementation IANotificationListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
