7a5bb8ea342b2543ed9ec3f04460fe1a3d8d6a52
Christopher Crone <christopher.crone@infinit.io>
2013-08-01 13:45:04 +0200

    Abstracted view controller class.

---
+++ InfinitApplication/IAViewController.h
@@ -0,0 +1,17 @@
+//
+//  IAViewController.h
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 7/31/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IAViewController : NSViewController
+
+@property (nonatomic, retain) IBOutlet NSView* footer_view;
+@property (nonatomic, retain) IBOutlet NSView* header_view;
+@property (nonatomic, retain) IBOutlet NSView* main_view;
+
+@end
---
+++ InfinitApplication/IAViewController.m
@@ -0,0 +1,31 @@
+//
+//  IAViewController.m
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 7/31/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import "IAViewController.h"
+
+@interface IAViewController ()
+
+@end
+
+@implementation IAViewController
+
+@synthesize header_view;
+@synthesize main_view;
+@synthesize footer_view;
+
+//- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+//{
+//    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+//    if (self) {
+//        // Initialization code here.
+//    }
+//    
+//    return self;
+//}
+
+@end
