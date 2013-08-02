0c916b1abd8efd68ad2655a98ae59a083f90bc32
Christopher Crone <christopher.crone@infinit.io>
2013-08-02 11:42:32 +0200

    Add required icons.

---
+++ InfinitApplication/IASimpleSendViewController.h
@@ -0,0 +1,13 @@
+//
+//  IASimpleSendViewController.h
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/2/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import "IAViewController.h"
+
+@interface IASimpleSendViewController : IAViewController
+
+@end
