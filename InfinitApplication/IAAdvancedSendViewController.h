aa036fb3fcf62d203ebadac956d34a9eb8845be6
Christopher Crone <christopher.crone@infinit.io>
2013-08-04 15:45:09 +0200

    Add initial work for Advanced send view.

---
+++ InfinitApplication/IAAdvancedSendViewController.h
@@ -0,0 +1,13 @@
+//
+//  IAAdvancedSendViewController.h
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/4/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import "IAViewController.h"
+
+@interface IAAdvancedSendViewController : IAViewController
+
+@end
---
+++ InfinitApplication/IAAdvancedSendViewController.m
@@ -0,0 +1,27 @@
+//
+//  IAAdvancedSendViewController.m
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/4/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import "IAAdvancedSendViewController.h"
+
+@interface IAAdvancedSendViewController ()
+
+@end
+
+@implementation IAAdvancedSendViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
