6e1927a29581380f685bdccc954894e27a4ea0a2
Christopher Crone <christopher.crone@infinit.io>
2013-08-05 14:48:28 +0200

    Class for handling file view cells of advanced view.

---
+++ InfinitApplication/IASendFileListCellView.h
@@ -0,0 +1,18 @@
+//
+//  IASendFileListCellView.h
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/5/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IASendFileListCellView : NSTableCellView
+
+@property (nonatomic, strong) IBOutlet NSTextField* file_name;
+@property (nonatomic, strong) IBOutlet NSTextField* file_size;
+@property (nonatomic, strong) IBOutlet NSImageView* file_type_image;
+@property (nonatomic, strong) IBOutlet NSButton* remove_file_button;
+
+@end
---
+++ InfinitApplication/IASendFileListCellView.m
@@ -0,0 +1,28 @@
+//
+//  IASendFileListCellView.m
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/5/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import "IASendFileListCellView.h"
+
+@implementation IASendFileListCellView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
