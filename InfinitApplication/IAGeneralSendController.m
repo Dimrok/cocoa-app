910cccdc59dc167d297379e9cf22407dfd96ac49
Christopher Crone <christopher.crone@infinit.io>
2013-08-08 18:48:21 +0200

    Update general send controller to interact with advanced controller.

--- InfinitApplication/IAGeneralSendController.m
+++ InfinitApplication/IAGeneralSendController.m
@@ -18,6 +18,7 @@
     IAAdvancedSendViewController* _advanced_send_controller;
     IASimpleSendViewController* _simple_send_controller;
     
+    id _currently_open_controller;
     IAUserSearchViewController* _user_search_controller;
     NSMutableArray* _files;
 }
@@ -39,23 +40,26 @@
 
 - (void)openWithNoFile
 {
-    _advanced_send_controller = nil;
     if (_simple_send_controller == nil)
         _simple_send_controller = [[IASimpleSendViewController alloc]
                                         initWithDelegate:self
                                      andSearchController:_user_search_controller];
     [_delegate sendController:self wantsActiveController:_simple_send_controller];
+    _currently_open_controller = _simple_send_controller;
 }
 
 - (void)openWithFiles:(NSArray*)files
 {
     [_files addObjectsFromArray:files];
-    if (_simple_send_controller == nil)
+    if (_currently_open_controller == nil)
+    {
         _simple_send_controller = [[IASimpleSendViewController alloc]
                                    initWithDelegate:self
                                    andSearchController:_user_search_controller];
+        _currently_open_controller = _simple_send_controller;
+    }
     else
-        [_simple_send_controller filesAdded];
+        [_currently_open_controller filesUpdated];
     [_delegate sendController:self wantsActiveController:_simple_send_controller];
 }
 
@@ -68,6 +72,8 @@
                                         initWithDelegate:self
                                      andSearchController:_user_search_controller];
     [_delegate sendController:self wantsActiveController:_advanced_send_controller];
+    _currently_open_controller = _advanced_send_controller;
+    _simple_send_controller = nil;
 }
 
 //- Simple Send View Protocol ----------------------------------------------------------------------
@@ -100,10 +106,22 @@
 
 //- Advanced Send View Protocol --------------------------------------------------------------------
 
+- (void)advancedSendViewWantsCancel:(IAAdvancedSendViewController*)sender
+{
+    _files = nil;
+    [_delegate sendControllerWantsClose:self];
+}
+
 - (NSArray*)advancedSendViewWantsFileList:(IAAdvancedSendViewController*)sender
 {
     return [NSArray arrayWithArray:_files];
 }
 
+- (void)advancedSendView:(IAAdvancedSendViewController*)sender
+  wantsRemoveFileAtIndex:(NSInteger)index
+{
+    [_files removeObjectAtIndex:index];
+    [sender filesUpdated];
+}
 
 @end
