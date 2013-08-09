7f5c797d7b4ba154d73b53c8820bfa83c9b6f2c0
Christopher Crone <christopher.crone@infinit.io>
2013-08-09 17:18:48 +0200

    Cell view for notifications.

---
+++ InfinitApplication/IANotificationListCellView.h
@@ -0,0 +1,13 @@
+//
+//  IANotificationListCellView.h
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/9/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IANotificationListCellView : NSTableCellView
+
+@end
---
+++ InfinitApplication/IANotificationListCellView.m
@@ -0,0 +1,28 @@
+//
+//  IANotificationListCellView.m
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/9/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import "IANotificationListCellView.h"
+
+@implementation IANotificationListCellView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
