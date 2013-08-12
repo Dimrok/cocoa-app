7dbf1fbf255e91e135b0f769bc6d9e92a63d6896
Christopher Crone <christopher.crone@infinit.io>
2013-08-12 11:53:22 +0200

    Set up a cell given a transaction.

--- InfinitApplication/IANotificationListCellView.h
+++ InfinitApplication/IANotificationListCellView.h
@@ -10,4 +10,12 @@
 
 @interface IANotificationListCellView : NSTableCellView
 
+@property (nonatomic, strong) IBOutlet NSImageView* avatar;
+@property (nonatomic, strong) IBOutlet NSTextField* file_name;
+@property (nonatomic, strong) IBOutlet NSTextField* time_since_change;
+@property (nonatomic, strong) IBOutlet NSImageView* transfer_status;
+@property (nonatomic, strong) IBOutlet NSTextField* user_full_name;
+
+- (void)setupCellWithTransaction:(IATransaction*)transaction;
+
 @end
--- InfinitApplication/IANotificationListCellView.m
+++ InfinitApplication/IANotificationListCellView.m
@@ -8,21 +8,100 @@
 
 #import "IANotificationListCellView.h"
 
+#import "IAAvatarManager.h"
+
 @implementation IANotificationListCellView
+{
+@private
+    NSInteger _avatar_size;
+}
 
 - (id)initWithFrame:(NSRect)frame
 {
-    self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code here.
+    if (self = [super initWithFrame:frame])
+    {
+        _avatar_size = 48.0;
     }
     
     return self;
 }
 
-- (void)drawRect:(NSRect)dirtyRect
+- (NSString*)description
 {
-    // Drawing code here.
+    return @"[NotificationListCell]";
+}
+
+- (void)setUserFullName:(NSString*)fullname
+{
+    NSDictionary* attrs = [IAFunctions textStyleWithFont:[NSFont boldSystemFontOfSize:12.0]
+                                          paragraphStyle:[NSParagraphStyle defaultParagraphStyle]
+                                                  colour:TH_RGBCOLOR(29.0, 29.0, 29.0)
+                                                  shadow:nil];
+    self.user_full_name.attributedStringValue = [[NSAttributedString alloc] initWithString:fullname
+                                                                                attributes:attrs];
+}
+
+- (void)setFileName:(NSString*)file_name
+{
+    NSDictionary* attrs = [IAFunctions textStyleWithFont:[NSFont systemFontOfSize:11.0]
+                                          paragraphStyle:[NSParagraphStyle defaultParagraphStyle]
+                                                  colour:TH_RGBCOLOR(153.0, 153.0, 153.0)
+                                                  shadow:nil];
+    NSString* res = file_name;
+    NSInteger str_len = 25;
+    if (res.length > str_len)
+    {
+        NSString* file_extension = [file_name pathExtension];
+        NSString* temp = [[file_name stringByDeletingPathExtension]
+                          substringToIndex:(str_len - file_extension.length)];
+        res = [temp stringByAppendingFormat:@"...%@", file_extension];
+    }
+    self.file_name.attributedStringValue = [[NSAttributedString alloc] initWithString:res
+                                                                           attributes:attrs];
+}
+
+- (void)setLastActionTime:(NSTimeInterval)timestamp
+{
+    NSDictionary* attrs = [IAFunctions textStyleWithFont:[NSFont systemFontOfSize:11.0]
+                                          paragraphStyle:[NSParagraphStyle defaultParagraphStyle]
+                                                  colour:TH_RGBCOLOR(206.0, 206.0, 206.0)
+                                                  shadow:nil];
+    NSDate* date = [NSDate dateWithTimeIntervalSince1970:timestamp];
+    NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
+    formatter.dateStyle = NSDateFormatterShortStyle;
+    formatter.timeStyle = NSDateFormatterShortStyle;
+    self.time_since_change.attributedStringValue = [[NSAttributedString alloc]
+                                                    initWithString:[formatter stringFromDate:date]
+                                                        attributes:attrs];
+}
+
+- (void)setAvatarForUser:(IAUser*)user
+{
+    NSImage* avatar = [IAFunctions makeRoundAvatar:[IAAvatarManager getAvatarForUser:user
+                                                                     andLoadIfNeeded:YES]
+                                          ofRadius:_avatar_size
+                                   withWhiteBorder:YES];
+    [self.avatar setWantsLayer:YES];
+    self.avatar.layer.shadowOffset = NSZeroSize;
+    self.avatar.layer.shadowRadius = 1.0;
+    self.avatar.layer.shadowOpacity = 0.36;
+    self.avatar.image = avatar;
+}
+
+- (void)setupCellWithTransaction:(IATransaction*)transaction
+{
+    if (transaction.from_me)
+    {
+        [self setUserFullName:transaction.recipient_fullname];
+        [self setAvatarForUser:[IAUser userWithId:transaction.recipient_id]];
+    }
+    else
+    {
+        [self setUserFullName:transaction.sender_fullname];
+        [self setAvatarForUser:[IAUser userWithId:transaction.sender_id]];
+    }
+    [self setFileName:transaction.first_filename];
+    [self setLastActionTime:transaction.timestamp];
 }
 
 @end
