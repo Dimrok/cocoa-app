5267bbb5e9d6dbd081332f92943d3e9f628fb65c
Christopher Crone <christopher.crone@infinit.io>
2013-08-13 16:02:06 +0200

    Initial work on favourites view.

---
+++ InfinitApplication/IAFavouritesViewController.h
@@ -0,0 +1,13 @@
+//
+//  IAFavouritesViewController.h
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/13/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import "IAViewController.h"
+
+@interface IAFavouritesViewController : IAViewController
+
+@end
---
+++ InfinitApplication/IAFavouritesViewController.m
@@ -0,0 +1,27 @@
+//
+//  IAFavouritesViewController.m
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/13/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import "IAFavouritesViewController.h"
+
+@interface IAFavouritesViewController ()
+
+@end
+
+@implementation IAFavouritesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
