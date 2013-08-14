7580cb0a2df2607ab17bbc5a428cdc9885685c8d
Christopher Crone <christopher.crone@infinit.io>
2013-08-14 12:02:45 +0200

    Begin separation of favourites view from controller.

---
+++ InfinitApplication/IAFavouritesView.h
@@ -0,0 +1,13 @@
+//
+//  IAFavouritesView.h
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/14/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IAFavouritesView : NSView
+
+@end
---
+++ InfinitApplication/IAFavouritesView.m
@@ -0,0 +1,28 @@
+//
+//  IAFavouritesView.m
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/14/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import "IAFavouritesView.h"
+
+@implementation IAFavouritesView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
