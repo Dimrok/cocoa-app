fe05827febccb40cd53baada2ba35df3b709f2be
Christopher Crone <christopher.crone@infinit.io>
2013-08-16 16:07:02 +0200

    Add file for conversation cell views.

---
+++ InfinitApplication/IAConversationCellView.h
@@ -0,0 +1,13 @@
+//
+//  IAConversationCellView.h
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/15/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IAConversationCellView : NSView
+
+@end
---
+++ InfinitApplication/IAConversationCellView.m
@@ -0,0 +1,28 @@
+//
+//  IAConversationCellView.m
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/15/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import "IAConversationCellView.h"
+
+@implementation IAConversationCellView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
