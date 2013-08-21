045833eefe50e8c083c8169bc048e4540b770ea1
Christopher Crone <christopher.crone@infinit.io>
2013-08-21 14:56:21 +0200

    Add initial work for badge views.

---
+++ InfinitApplication/IAAvatarBadgeView.h
@@ -0,0 +1,13 @@
+//
+//  IAAvatarBadgeView.h
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/21/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IAAvatarBadgeView : NSView
+
+@end
---
+++ InfinitApplication/IAAvatarBadgeView.m
@@ -0,0 +1,28 @@
+//
+//  IAAvatarBadgeView.m
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/21/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import "IAAvatarBadgeView.h"
+
+@implementation IAAvatarBadgeView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
