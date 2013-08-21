edb70d83d3a3d476cac84469a1120cdf32f44665
Christopher Crone <christopher.crone@infinit.io>
2013-08-21 20:15:51 +0200

    Initial work on notification avatar view.

---
+++ InfinitApplication/IANotificationAvatarView.h
@@ -0,0 +1,13 @@
+//
+//  IANotificationAvatarView.h
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/21/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IANotificationAvatarView : NSView
+
+@end
---
+++ InfinitApplication/IANotificationAvatarView.m
@@ -0,0 +1,28 @@
+//
+//  IANotificationAvatarView.m
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/21/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import "IANotificationAvatarView.h"
+
+@implementation IANotificationAvatarView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
