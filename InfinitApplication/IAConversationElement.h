74534e6b64878bff63339cd2c39bc183db96a844
Christopher Crone <christopher.crone@infinit.io>
2013-08-26 18:29:37 +0200

    Add icons for conversation view.

---
+++ InfinitApplication/IAConversationElement.h
@@ -0,0 +1,13 @@
+//
+//  IAConversationElement.h
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/26/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IAConversationElement : NSObject
+
+@end
---
+++ InfinitApplication/IAConversationElement.m
@@ -0,0 +1,13 @@
+//
+//  IAConversationElement.m
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/26/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import "IAConversationElement.h"
+
+@implementation IAConversationElement
+
+@end
