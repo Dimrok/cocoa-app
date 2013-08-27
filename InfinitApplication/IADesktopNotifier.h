727cc2eeb374d04ae75791e7f5ad182ba2c20584
Christopher Crone <christopher.crone@infinit.io>
2013-08-27 15:08:01 +0200

    Add files for desktop notifier

---
+++ InfinitApplication/IADesktopNotifier.h
@@ -0,0 +1,13 @@
+//
+//  IADesktopNotifier.h
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/27/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IADesktopNotifier : NSObject
+
+@end
---
+++ InfinitApplication/IADesktopNotifier.m
@@ -0,0 +1,13 @@
+//
+//  IADesktopNotifier.m
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 8/27/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import "IADesktopNotifier.h"
+
+@implementation IADesktopNotifier
+
+@end
