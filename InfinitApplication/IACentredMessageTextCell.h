877f4223de6e77915c1524c41270b7cde4039e82
Christopher Crone <christopher.crone@infinit.io>
2013-10-03 13:28:14 +0200

    Add centred message text cell for conversation view messages.

---
+++ InfinitApplication/IACentredMessageTextCell.h
@@ -0,0 +1,13 @@
+//
+//  IACentredMessageTextCell.h
+//  InfinitApplication
+//
+//  Created by Christopher Crone on 10/3/13.
+//  Copyright (c) 2013 Infinit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IACentredMessageTextCell : NSTextFieldCell
+
+@end
