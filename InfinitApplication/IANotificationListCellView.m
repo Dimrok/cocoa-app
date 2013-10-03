dd744d641e3caadc55d9ae524759ad23a2b6ccaa
Christopher Crone <christopher.crone@infinit.io>
2013-10-03 13:31:18 +0200

    Change name font size and correct positioning.

--- InfinitApplication/IANotificationListCellView.m
+++ InfinitApplication/IANotificationListCellView.m
@@ -29,7 +29,7 @@
     NSFont* name_font = [[NSFontManager sharedFontManager] fontWithFamily:@"Helvetica"
                                                                    traits:NSUnboldFontMask
                                                                    weight:7
-                                                                     size:12.0];
+                                                                     size:13.0];
     NSDictionary* attrs = [IAFunctions textStyleWithFont:name_font
                                           paragraphStyle:[NSParagraphStyle defaultParagraphStyle]
                                                   colour:IA_GREY_COLOUR(29.0)
