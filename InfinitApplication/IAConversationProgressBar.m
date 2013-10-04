3d0608a2380dcda785a990c486e780f89c859bda
Christopher Crone <christopher.crone@infinit.io>
2013-10-04 14:05:28 +0200

    Adjust progress bar colour.

--- InfinitApplication/IAConversationProgressBar.m
+++ InfinitApplication/IAConversationProgressBar.m
@@ -63,7 +63,7 @@
                                            border_thickness,
                                       NSHeight(self.bounds) - 2.0 * border_thickness);
     NSBezierPath* progress_bar = [NSBezierPath bezierPathWithRect:progress_rect];
-    [IA_RGB_COLOUR(216.0, 237.0, 243.0) set];
+    [IA_RGB_COLOUR(202.0, 217.0, 223.0) set];
     [progress_bar fill];
     NSBezierPath* progress_line = [NSBezierPath bezierPathWithRect:
                                    NSMakeRect(progress_rect.origin.x + progress_rect.size.width,
