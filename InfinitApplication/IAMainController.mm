d1d615e984de9d2f65d2e3e4fd15cdd217c46944
Christopher Crone <christopher.crone@infinit.io>
2014-04-12 19:18:55 -0400

    Allow drag and drop on favourites while onboarding.

--- InfinitApplication/IAMainController.mm
+++ InfinitApplication/IAMainController.mm
@@ -1237,8 +1237,7 @@ transactionsProgressForUser:(IAUser*)user
 - (void)statusBarIconDragEntered:(IAStatusBarIcon*)sender
 {
   if (![[IAGapState instance] logged_in] ||
-      [_me_manager connection_status] != gap_user_status_online ||
-      [self onboardingState:nil] != INFINIT_ONBOARDING_DONE)
+      [_me_manager connection_status] != gap_user_status_online)
   {
     return;
   }
