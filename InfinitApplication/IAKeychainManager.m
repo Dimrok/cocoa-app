74fe8537e044494876a0fb4dba4f5f4e5931ea4d
Christopher Crone <christopher.crone@infinit.io>
2014-06-13 12:32:50 +0200

    Norm.

--- InfinitApplication/IAKeychainManager.m
+++ InfinitApplication/IAKeychainManager.m
@@ -17,8 +17,8 @@
 @implementation IAKeychainManager
 {
 @private
-    NSString* _service_name;
-    NSString* _application_path;
+  NSString* _service_name;
+  NSString* _application_path;
 }
 
 //- Initialisation ---------------------------------------------------------------------------------
@@ -27,20 +27,20 @@
 {
 	static IAKeychainManager* keychain_manager = nil;
 	if (keychain_manager == nil)
-    {
+  {
 		keychain_manager = [[IAKeychainManager alloc] init];
-    }
-    return keychain_manager;
+  }
+  return keychain_manager;
 }
 
 - (id)init
 {
 	if (self = [super init])
-    {
-        _service_name = @"Infinit";
-        _application_path = [IAGapState.instance.protocol getApplicationPath];
-        
-    }
+  {
+    _service_name = @"Infinit";
+    _application_path = [IAGapState.instance.protocol getApplicationPath];
+
+  }
 	return self;
 }
 
@@ -53,59 +53,59 @@
                  passwordLength:(UInt32*)password_length
                         itemRef:(SecKeychainItemRef*)item_ref
 {
-    OSStatus status;
-    
-    status = SecKeychainFindGenericPassword(
-                                            NULL, // default keychain
-                                            (UInt32)_service_name.length, // length of service name
-                                            _service_name.UTF8String, // service name
-                                            (UInt32)user_email.length, // length of account name
-                                            user_email.UTF8String,   // account name
-                                            password_length, // length of password
-                                            password_data, // pointer to password data
-                                            item_ref // the item reference
-                                            );
-    
-    return status;
+  OSStatus status;
+
+  status = SecKeychainFindGenericPassword(
+    NULL, // default keychain
+    (UInt32)_service_name.length, // length of service name
+    _service_name.UTF8String,     // service name
+    (UInt32)user_email.length,    // length of account name
+    user_email.UTF8String,        // account name
+    password_length,              // length of password
+    password_data,                // pointer to password data
+    item_ref                      // the item reference
+  );
+
+  return status;
 }
 
 // Check if credentials are in keychain
 - (BOOL)credentialsInKeychain:(NSString*)email_address
 {
-    if ([self getPasswordKeychain:email_address
-                     passwordData:NULL
-                   passwordLength:NULL
-                          itemRef:NULL] == noErr)
-    {
-        return YES;
-    }
-    else
-    {
-        return NO;
-    }
+  if ([self getPasswordKeychain:email_address
+                   passwordData:NULL
+                 passwordLength:NULL
+                        itemRef:NULL] == noErr)
+  {
+    return YES;
+  }
+  else
+  {
+    return NO;
+  }
 }
 
 // Update user's password in keychain
 - (OSStatus)changeUser:(NSString*)user_email
               password:(NSString*)password
 {
-    OSStatus status = noErr;
-    SecKeychainItemRef item_ref = NULL;
-    
-    status = [self getPasswordKeychain:user_email
-                          passwordData:NULL
-                        passwordLength:0
-                               itemRef:&item_ref];
-    if (status != noErr)
-        return status;
-    
-    status = SecKeychainItemModifyContent(item_ref,
-                                          NULL,
-                                          (UInt32)password.length,
-                                          password.UTF8String);
-    password = @"";
-    password = nil;
+  OSStatus status = noErr;
+  SecKeychainItemRef item_ref = NULL;
+
+  status = [self getPasswordKeychain:user_email
+                        passwordData:NULL
+                      passwordLength:0
+                             itemRef:&item_ref];
+  if (status != noErr)
     return status;
+
+  status = SecKeychainItemModifyContent(item_ref,
+                                        NULL,
+                                        (UInt32)password.length,
+                                        password.UTF8String);
+  password = @"";
+  password = nil;
+  return status;
 }
 
 // Add credentials to keychain
@@ -113,45 +113,45 @@
 - (OSStatus)addPasswordKeychain:(NSString*)user_email
                        password:(NSString*)password
 {
-    OSStatus status;
-    SecAccessRef access;
-    NSArray* trusted_applications = nil;
-    
-    SecTrustedApplicationRef infinit_app;
-    status = SecTrustedApplicationCreateFromPath(_application_path.UTF8String, &infinit_app);
-    if (status != noErr)
-        return status;
-    
-    trusted_applications = [NSArray arrayWithObject:(__bridge id)infinit_app];
-    
-    status = SecAccessCreate((__bridge CFStringRef)_service_name,
-                             (__bridge CFArrayRef)(trusted_applications),
-                             &access);
-    if (status != noErr)
-        return status;
-    
-    SecKeychainAttribute attrs[] = {
-        { kSecLabelItemAttr, (UInt32)_service_name.length, (void*)_service_name.UTF8String },
-        { kSecAccountItemAttr, (UInt32)user_email.length, (void*)user_email.UTF8String },
-        { kSecServiceItemAttr, (UInt32)_service_name.length, (void*)_service_name.UTF8String }
-    };
-    
-    SecKeychainAttributeList attributes = { sizeof(attrs) / sizeof(attrs[0]), attrs };
-    
-    status = SecKeychainItemCreateFromContent(
-                                              kSecGenericPasswordItemClass,
-                                              &attributes,
-                                              (UInt32)password.length,
-                                              password.UTF8String,
-                                              NULL, // use the default keychain
-                                              access,
-                                              NULL
-                                             );
-    if (access)
-        CFRelease(access);
-    password = @"";
-    password = nil;
+  OSStatus status;
+  SecAccessRef access;
+  NSArray* trusted_applications = nil;
+
+  SecTrustedApplicationRef infinit_app;
+  status = SecTrustedApplicationCreateFromPath(_application_path.UTF8String, &infinit_app);
+  if (status != noErr)
     return status;
+
+  trusted_applications = [NSArray arrayWithObject:(__bridge id)infinit_app];
+
+  status = SecAccessCreate((__bridge CFStringRef)_service_name,
+                           (__bridge CFArrayRef)(trusted_applications),
+                           &access);
+  if (status != noErr)
+    return status;
+
+  SecKeychainAttribute attrs[] = {
+    { kSecLabelItemAttr, (UInt32)_service_name.length, (void*)_service_name.UTF8String },
+    { kSecAccountItemAttr, (UInt32)user_email.length, (void*)user_email.UTF8String },
+    { kSecServiceItemAttr, (UInt32)_service_name.length, (void*)_service_name.UTF8String }
+  };
+
+  SecKeychainAttributeList attributes = { sizeof(attrs) / sizeof(attrs[0]), attrs };
+
+  status = SecKeychainItemCreateFromContent(
+                                            kSecGenericPasswordItemClass,
+                                            &attributes,
+                                            (UInt32)password.length,
+                                            password.UTF8String,
+                                            NULL, // use the default keychain
+                                            access,
+                                            NULL
+                                            );
+  if (access)
+    CFRelease(access);
+  password = @"";
+  password = nil;
+  return status;
 }
 
 @end
