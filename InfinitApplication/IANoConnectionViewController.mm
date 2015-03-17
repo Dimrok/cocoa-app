a622f0eeddb7718f2ad47cfc40052ab3c172e156
Christopher Crone <christopher.crone@infinit.io>
2015-03-17 19:03:39 +0100

    NoConnectionViewController: Norm.

--- InfinitApplication/IANoConnectionViewController.mm
+++ InfinitApplication/IANoConnectionViewController.mm
@@ -24,14 +24,14 @@ ELLE_LOG_COMPONENT("OSX.NoConnectionViewController");
 
 - (void)drawRect:(NSRect)dirtyRect
 {
-    NSBezierPath* path = [NSBezierPath bezierPathWithRect:self.bounds];
-    [IA_GREY_COLOUR(248.0) set];
-    [path fill];
+  NSBezierPath* path = [NSBezierPath bezierPathWithRect:self.bounds];
+  [IA_GREY_COLOUR(248.0) set];
+  [path fill];
 }
 
 - (NSSize)intrinsicContentSize
 {
-    return self.bounds.size;
+  return self.bounds.size;
 }
 
 @end
@@ -39,57 +39,57 @@ ELLE_LOG_COMPONENT("OSX.NoConnectionViewController");
 @implementation IANoConnectionViewController
 {
 @private
-    id<IANoConnectionViewProtocol> _delegate;
+  id<IANoConnectionViewProtocol> _delegate;
 }
 
 //- Initialisation ---------------------------------------------------------------------------------
 
 - (id)initWithDelegate:(id<IANoConnectionViewProtocol>)delegate
 {
-    if (self = [super initWithNibName:[self className] bundle:nil])
-    {
-        _delegate = delegate;
-    }
-    return self;
+  if (self = [super initWithNibName:[self className] bundle:nil])
+  {
+    _delegate = delegate;
+  }
+  return self;
 }
 
 - (BOOL)closeOnFocusLost
 {
-    return YES;
+  return YES;
 }
 
 - (void)awakeFromNib
 {
-    NSMutableParagraphStyle* style = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
-    style.alignment = NSCenterTextAlignment;
-    NSFont* font = [[NSFontManager sharedFontManager] fontWithFamily:@"Helvetica"
-                                                              traits:NSUnboldFontMask
-                                                              weight:0
-                                                                size:12.0];
-    NSDictionary* message_attrs = [IAFunctions textStyleWithFont:font
-                                                  paragraphStyle:style
-                                                          colour:IA_GREY_COLOUR(32.0)
-                                                          shadow:nil];
-    NSString* message = NSLocalizedString(@"Need an Internet connection to send...",
-                                          @"need an internet connection to send");
-    
-    self.no_connection_message.attributedStringValue = [[NSAttributedString alloc]
-                                                        initWithString:message
-                                                            attributes:message_attrs];
+  NSMutableParagraphStyle* style = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
+  style.alignment = NSCenterTextAlignment;
+  NSFont* font = [[NSFontManager sharedFontManager] fontWithFamily:@"Helvetica"
+                                                            traits:NSUnboldFontMask
+                                                            weight:0
+                                                              size:12.0];
+  NSDictionary* message_attrs = [IAFunctions textStyleWithFont:font
+                                                paragraphStyle:style
+                                                        colour:IA_GREY_COLOUR(32.0)
+                                                        shadow:nil];
+  NSString* message = NSLocalizedString(@"Need an Internet connection to send...",
+                                        @"need an internet connection to send");
+
+  self.no_connection_message.attributedStringValue = [[NSAttributedString alloc]
+                                                      initWithString:message
+                                                      attributes:message_attrs];
 }
 
 - (void)loadView
 {
-    ELLE_TRACE("%s: loadview", self.description.UTF8String);
-    [super loadView];
-    [self.view layoutSubtreeIfNeeded];
+  ELLE_TRACE("%s: loadview", self.description.UTF8String);
+  [super loadView];
+  [self.view layoutSubtreeIfNeeded];
 }
 
 //- User Interaction -------------------------------------------------------------------------------
 
 - (IBAction)backButtonClicked:(NSButton*)sender
 {
-    [_delegate noConnectionViewWantsBack:self];
+  [_delegate noConnectionViewWantsBack:self];
 }
 
 @end
