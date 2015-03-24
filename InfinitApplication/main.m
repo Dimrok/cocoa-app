717c54375c654bb076538b5bd6455fdbae308133
Christopher Crone <christopher.crone@infinit.io>
2015-03-24 18:33:28 +0100

    main: Ensure that the DYLD_ROOT_PATH is set correctly on launch.

--- InfinitApplication/main.m
+++ InfinitApplication/main.m
@@ -10,5 +10,16 @@
 
 int main(int argc, char *argv[])
 {
+#ifndef DEBUG
+  if (!getenv("DYLD_ROOT_PATH"))
+  {
+    @autoreleasepool
+    {
+      NSString* our_cxx_path = [NSBundle mainBundle].privateFrameworksPath;
+      setenv("DYLD_ROOT_PATH", our_cxx_path.UTF8String, 1);
+    }
+    execvp(argv[0], argv);
+  }
+#endif
   return NSApplicationMain(argc, (const char **)argv);
 }
