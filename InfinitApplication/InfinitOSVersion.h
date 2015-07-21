2d6a247bc0e0334c09e41b5773afe15fea6dc590
Christopher Crone <christopher.crone@infinit.io>
2015-07-21 18:18:37 +0200

    OSVersion: Handle OS version related things here.

--- InfinitApplication/InfinitOSVersion.h
+++ InfinitApplication/InfinitOSVersion.h
@@ -13,8 +13,27 @@ typedef struct _InfinitOSVersionStruct
   SInt32 major, minor, subminor;
 } InfinitOSVersionStruct;
 
+InfinitOSVersionStruct
+InfinitOSVersionMake(int major, int minor, int subminor);
+
+InfinitOSVersionStruct
+InfinitOSReleaseMake(int major, int minor);
+
 @interface InfinitOSVersion : NSObject
 
 + (InfinitOSVersionStruct)osVersion;
++ (NSString*)osVersionString;
+
+/// Check only major and minor version numbers.
++ (BOOL)equalToRelease:(InfinitOSVersionStruct)version;
++ (BOOL)greaterThanRelease:(InfinitOSVersionStruct)version;
++ (BOOL)greaterThanEqualToRelease:(InfinitOSVersionStruct)version;
++ (BOOL)lessThanRelease:(InfinitOSVersionStruct)version;
+
+/// Exact version checking.
++ (BOOL)equalTo:(InfinitOSVersionStruct)version;
++ (BOOL)greaterThan:(InfinitOSVersionStruct)version;
++ (BOOL)greaterThanEqualTo:(InfinitOSVersionStruct)version;
++ (BOOL)lessThan:(InfinitOSVersionStruct)version;
 
 @end
--- InfinitApplication/InfinitOSVersion.m
+++ InfinitApplication/InfinitOSVersion.m
@@ -18,18 +18,124 @@ InfinitOSVersionMake(int major, int minor, int subminor)
   return res;
 }
 
+InfinitOSVersionStruct
+InfinitOSReleaseMake(int major, int minor)
+{
+  return InfinitOSVersionMake(major, minor, 0);
+}
+
+static InfinitOSVersionStruct _version;
+static dispatch_once_t _version_token = 0;
+
 @implementation InfinitOSVersion
+
+#pragma mark - Version
 
 + (InfinitOSVersionStruct)osVersion
 {
-  SInt32 major, minor, subminor;
-  if (Gestalt(gestaltSystemVersionMajor, &major) != noErr)
-    return InfinitOSVersionMake(0, 0, 0);
-  if (Gestalt(gestaltSystemVersionMinor, &minor) != noErr)
-    return InfinitOSVersionMake(0, 0, 0);
-  if (Gestalt(gestaltSystemVersionBugFix, &subminor) != noErr)
-    return InfinitOSVersionMake(0, 0, 0);
-  return InfinitOSVersionMake(major, minor, subminor);
+  dispatch_once(&_version_token, ^
+  {
+    SInt32 major, minor, subminor;
+    if (Gestalt(gestaltSystemVersionMajor, &major) != noErr)
+    {
+      _version = InfinitOSVersionMake(0, 0, 0);
+      _version_token = 0;
+    }
+    if (Gestalt(gestaltSystemVersionMinor, &minor) != noErr)
+    {
+      _version = InfinitOSVersionMake(0, 0, 0);
+      _version_token = 0;
+    }
+    if (Gestalt(gestaltSystemVersionBugFix, &subminor) != noErr)
+    {
+      _version = InfinitOSVersionMake(0, 0, 0);
+      _version_token = 0;
+    }
+    _version = InfinitOSVersionMake(major, minor, subminor);
+  });
+  return _version;
+}
+
++ (NSString*)osVersionString
+{
+  InfinitOSVersionStruct version = [self osVersion];
+  return [NSString stringWithFormat:@"%d.%d.%d", version.major, version.minor, version.subminor];
+}
+
+#pragma mark - Release Comparison
+
++ (BOOL)equalToRelease:(InfinitOSVersionStruct)version
+{
+  InfinitOSVersionStruct this_version = [self osVersion];
+  if (this_version.major == version.major && this_version.minor == version.minor)
+    return YES;
+  return NO;
+}
+
++ (BOOL)greaterThanRelease:(InfinitOSVersionStruct)version
+{
+  InfinitOSVersionStruct this_version = [self osVersion];
+  if (version.major > this_version.major)
+    return YES;
+  if (version.major == this_version.major && version.minor > this_version.minor)
+    return YES;
+  return NO;
+}
+
++ (BOOL)greaterThanEqualToRelease:(InfinitOSVersionStruct)version
+{
+  if (![self lessThanRelease:version])
+    return YES;
+  return NO;
+}
+
++ (BOOL)lessThanRelease:(InfinitOSVersionStruct)version
+{
+  if (![self greaterThanRelease:version] && ![self equalToRelease:version])
+    return YES;
+  return NO;
+}
+
++ (BOOL)equalTo:(InfinitOSVersionStruct)version
+{
+  InfinitOSVersionStruct this_version = [self osVersion];
+  if (this_version.major == version.major &&
+      this_version.minor == version.minor &&
+      this_version.subminor == version.subminor)
+  {
+    return YES;
+  }
+  return NO;
+}
+
+#pragma mark - Release Comparison
+
++ (BOOL)greaterThan:(InfinitOSVersionStruct)version
+{
+  InfinitOSVersionStruct this_version = [self osVersion];
+  if (version.major > this_version.major)
+    return YES;
+  if (version.major == this_version.major && version.minor > this_version.minor)
+    return YES;
+  if (version.major == this_version.major &&
+      version.minor == this_version.minor &&
+      version.subminor > this_version.subminor)
+  {
+    return YES;
+  }
+  return NO;
+}
+
++ (BOOL)greaterThanEqualTo:(InfinitOSVersionStruct)version
+{
+  return ![self lessThan:version];
+}
+
++ (BOOL)lessThan:(InfinitOSVersionStruct)version
+{
+  if (![self greaterThan:version] && ![self equalTo:version])
+    return YES;
+  return NO;
 }
 
 @end
